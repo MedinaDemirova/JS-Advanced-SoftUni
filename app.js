e480fce36b8338c11895920ed222fc48d52805f7
Medina Demirova <71286672+MedinaDemirova@users.noreply.github.com>
2020-11-01 23:34:59 +0200

    03 PhoneBook
    JS Advanced Applications REST Services

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-function solve() {
-    let current = 'depot';
-    let infoEl = document.getElementsByClassName('info')[0];
-    let departBtn = document.getElementById('depart');
-    let arriveBtn = document.getElementById('arrive');
-    let stopName;
-    let nextStopId;
+function attachEvents() {
+    let loadBtn = document.getElementById('btnLoad');
+    let phonebookUL = document.getElementById('phonebook');
 
-    function depart() {
-        let url = `https://judgetests.firebaseio.com/schedule/${current}.json`;
-        fetch(url)
+    loadBtn.addEventListener('click', () => {
+        phonebookUL.innerHTML = '';
+        fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
             .then(responce => responce.json())
             .then(data => {
-                if (typeof data === "object" && data !== null) {
-                    stopName = data.name;
-                    nextStopId = data.next;
-                    departBtn.disabled = "disabled";
-                    arriveBtn.removeAttribute("disabled");
-                    current = nextStopId;
-                    infoEl.innerHTML = `Next stop ${stopName}`;
-                } else {
-                    infoEl.innerHTML = "Error";
-                    departBtn.disabled = "disabled";
+                let kvp = Object.entries(data);
+                for (const iterator of kvp) {
+                    let newLI = document.createElement('li');
+                    newLI.innerHTML = `${iterator[1].person}: ${iterator[1].phone}`;
+                    let deleteBtn = document.createElement('button');
+                    deleteBtn.innerHTML = "Delete";
+                    newLI.appendChild(deleteBtn);
+                    phonebookUL.appendChild(newLI);
+                    deleteBtn.addEventListener('click', () => {
+                        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${iterator[0]}.json`, {
+                            method: 'DELETE'
+                        });
+                    });
                 };
             });
-    };
+    });
 
-    function arrive() {
-        infoEl.innerHTML = `Arriving at ${stopName}`;
-        departBtn.removeAttribute("disabled");
-        arriveBtn.disabled = "true";
-        current = nextStopId;
-    };
+    let createBtn = document.getElementById('btnCreate');
+    let nameInput = document.getElementById('person');
+    let phoneInput = document.getElementById('phone');
 
-    return {
-        depart,
-        arrive
-    };
+    createBtn.addEventListener('click', () => {
+        if (nameInput.value !== '' && phoneInput.value !== '') {
+            fetch('https://phonebook-nakov.firebaseio.com/phonebook.json', {
+                method: 'POST',
+                body: JSON.stringify({
+                    person: nameInput.value,
+                    phone: phoneInput.value,
+                })
+            });
+            nameInput.value = '';
+            phoneInput.value = '';
+            let event = document.createEvent('Event');
+            event.initEvent('click', true, false);
+            loadBtn.dispatchEvent(event);
+        };
+    });
 };
-
-let result = solve();
+attachEvents();
