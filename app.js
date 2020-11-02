2a150148a61eaeaadfd8d78c5c7cc70e3da03c66
Medina Demirova <71286672+MedinaDemirova@users.noreply.github.com>
2020-11-02 13:15:10 +0200

    04 Messenger
    EXERCISE: REST SERVICES AND AJAX

--- app.js
+++ app.js
@@ -1,48 +1,38 @@
 function attachEvents() {
-    let loadBtn = document.getElementById('btnLoad');
-    let phonebookUL = document.getElementById('phonebook');
+    let sendBtn = document.getElementById('submit');
+    let nameInput = document.getElementById('author');
+    let messageInput = document.getElementById('content');
+    let refreshBtn = document.getElementById('refresh');
+    let textArea = document.getElementById('messages');
 
-    loadBtn.addEventListener('click', () => {
-        phonebookUL.innerHTML = '';
-        fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
+
+    sendBtn.addEventListener('click', () => {
+        if (nameInput.value !== '' && messageInput.value !== '') {
+            fetch('https://rest-messanger.firebaseio.com/messanger.json', {
+                method: 'POST',
+                body: JSON.stringify({
+                    author: nameInput.value,
+                    content: messageInput.value,
+                })
+            });
+            nameInput.value = '';
+            messageInput.value = '';
+        }
+    });
+
+    refreshBtn.addEventListener('click', () => {
+        textArea.disabled = "false";
+        textArea.innerHTML = '';
+        fetch('https://rest-messanger.firebaseio.com/messanger.json')
             .then(responce => responce.json())
             .then(data => {
                 let kvp = Object.entries(data);
                 for (const iterator of kvp) {
-                    let newLI = document.createElement('li');
-                    newLI.innerHTML = `${iterator[1].person}: ${iterator[1].phone}`;
-                    let deleteBtn = document.createElement('button');
-                    deleteBtn.innerHTML = "Delete";
-                    newLI.appendChild(deleteBtn);
-                    phonebookUL.appendChild(newLI);
-                    deleteBtn.addEventListener('click', () => {
-                        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${iterator[0]}.json`, {
-                            method: 'DELETE'
-                        });
-                    });
-                };
+                    textArea.innerHTML += `${iterator[1].author}: ${iterator[1].content}\n`;
+                }
             });
     });
 
-    let createBtn = document.getElementById('btnCreate');
-    let nameInput = document.getElementById('person');
-    let phoneInput = document.getElementById('phone');
+}
 
-    createBtn.addEventListener('click', () => {
-        if (nameInput.value !== '' && phoneInput.value !== '') {
-            fetch('https://phonebook-nakov.firebaseio.com/phonebook.json', {
-                method: 'POST',
-                body: JSON.stringify({
-                    person: nameInput.value,
-                    phone: phoneInput.value,
-                })
-            });
-            nameInput.value = '';
-            phoneInput.value = '';
-            let event = document.createEvent('Event');
-            event.initEvent('click', true, false);
-            loadBtn.dispatchEvent(event);
-        };
-    });
-};
 attachEvents();
